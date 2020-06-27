2e3466acd9c84ffccdedea8c88d661c4dd48592c
Haley Collard <collard.haley20@gmail.com>
2020-06-27 11:13:42 -0400

    fix first letter bug

--- hangman.py
+++ hangman.py
@@ -3,29 +3,6 @@ import re
 import sys
 import requests
 
-"""
-A Hangman-style Dad Joke game!
-
-TODO:
-- setup:
-    - [DONE]obtain a random phrases - dad jokes API (https://icanhazdadjoke.com/)
-    - [DONE]initialize number of chances(6)
-    - [DONE]split joke and punch line
-    - [DONE]represent the phrase as a series of underscores
-    - display the outcome (the person figure)
-    - [DONE]maintain a list of guessed letters
-- game loop:
-    [DONE]- prompt user to enter a letter
-    - [DONE]determine whether the users guess is in the phrase
-    - [DONE]provide feedback about the users guess
-        - [DONE]if correct, display letter where it belongs in the phrase
-        - [DONE]if incorrect, display the next body part
-    - [DONE]win: fill the phrase
-    - [DONE]lose: chances = 0
--[DONE] display all guessed letters
-- [DONE]case insensitive bug
-TODO: figure out how to make it a two person competing game
-"""
 body_dict = {}
 body_dict[6] = (f'--|')
 body_dict[5] = (f'{body_dict[6]}\n  O')
@@ -36,12 +13,12 @@ body_dict[1] = (f'{body_dict[2]}\n /')
 body_dict[0] = (f'{body_dict[1]} \\')
 guessed_letters = []
 wrong_letters = []
-joke_requests_remaining = 3
-joke = 'What does an angry pepper do?'
-punchline = 'It gets jalopeno face.'
+# joke = 'What does an angry pepper do?'
+# punchline = 'It gets jalopeno face.'
 
 
 def get_joke():
+    joke_requests_remaining = 3
     headers = {'accept': 'application/json'}
     for _ in range(joke_requests_remaining):
         response = requests.get('https://icanhazdadjoke.com/', headers=headers)
@@ -56,6 +33,8 @@ def get_joke():
     # discard joke if there is no distinct punchline
     print(
         f'Did not get a good joke. {joke_requests_remaining} tries left.')
+    joke_requests_remaining -= 1
+    get_joke()
     sys.exit(1)
 
 
@@ -81,14 +60,14 @@ def main():
         gameboard = update_blanks(punchline)
         if '_' not in gameboard:
             print(punchline)
-            print(f'\nNice One! Laugh Away!!\n')
+            print(f'\nNice Job! Laugh Away!!\n')
             break
         print(gameboard)
         print(f'\nNumber of guesses remaining...{num_of_chances_remaining}\n')
         print(body_dict[num_of_chances_remaining])
-        print(f"\nPrevious guesses: {' '.join(wrong_letters)}")
+        print(f"\nIncorrect guesses: {' '.join(wrong_letters)}")
         guess = input("Guess a letter: ").lower()
-        if guess not in punchline:
+        if guess not in punchline.lower():
             wrong_letters.append(guess)
             if guess not in guessed_letters:
                 num_of_chances_remaining -= 1
