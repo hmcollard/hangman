3ff13f4d6ccf1daa23c00ac5974af322ea43dfb7
Haley Collard <collard.haley20@gmail.com>
2020-06-27 18:28:25 -0400

    fix joke attempt error

--- hangman.py
+++ hangman.py
@@ -15,26 +15,24 @@ guessed_letters = []
 wrong_letters = []
 # joke = 'What does an angry pepper do?'
 # punchline = 'It gets jalopeno face.'
+joke_requests_remaining = 3
 
 
 def get_joke():
-    joke_requests_remaining = 3
     headers = {'accept': 'application/json'}
     for _ in range(joke_requests_remaining):
         response = requests.get('https://icanhazdadjoke.com/', headers=headers)
         data = response.json()
         text = data['joke']
     # restrict joke format from API response to two part joke
-    joke_match = re.search(r'(.+[.?!])(.+[.?!])', text)
-    if joke_match:
-        joke = joke_match.group(1).strip()
-        punchline = joke_match.group(2).strip()
-        return joke, punchline
+        joke_match = re.search(r'(.+[.?!])(.+[.?!])', text)
+        if joke_match:
+            joke = joke_match.group(1).strip()
+            punchline = joke_match.group(2).strip()
+            return joke, punchline
     # discard joke if there is no distinct punchline
     print(
         f'Did not get a good joke. {joke_requests_remaining} tries left.')
-    joke_requests_remaining -= 1
-    get_joke()
     sys.exit(1)
 
 
