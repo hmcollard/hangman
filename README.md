d196211112e4324016c45b8fe5275cf60e9ca82c
Haley Collard <collard.haley20@gmail.com>
2020-06-30 21:03:34 -0400

    added score keeping feature and option to start new game

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 """
 A Hangman-style Dad Joke game!
 
-TODO:
+"""
 
-- setup:
-  - [DONE]obtain a random phrases - dad jokes API (https://icanhazdadjoke.com/)
-  - [DONE]initialize number of chances(6)
-  - [DONE]split joke and punch line
-  - [DONE]represent the phrase as a series of underscores
-  - [DONE]display the outcome (the person figure)
-  - [DONE]maintain a list of guessed letters
-- game loop:
-  [DONE]- prompt user to enter a letter - [DONE]determine whether the users guess is in the phrase - [DONE]provide feedback about the users guess - [DONE]if correct, display letter where it belongs in the phrase - [DONE]if incorrect, display the next body part - [DONE]win: fill the phrase - [DONE]lose: chances = 0 -[DONE] display all guessed letters
-- [DONE]case insensitive bug
-  TODO: figure out how to make it a two person competing game
-  """
+- Set HOW To Play game and Rules
--- hangman.py
+++ hangman.py
@@ -6,7 +6,7 @@ import requests
 
 # game settings
 body_dict = {}
-body_dict[6] = (f'--|')
+body_dict[6] = ('--|')
 body_dict[5] = (f'{body_dict[6]}\n  O')
 body_dict[4] = (f'{body_dict[5]}\n /')
 body_dict[3] = (f'{body_dict[4]}|')
@@ -15,9 +15,22 @@ body_dict[1] = (f'{body_dict[2]}\n /')
 body_dict[0] = (f'{body_dict[1]} \\')
 guessed_letters = []
 wrong_letters = []
-joke_requests_remaining = 3
+joke_requests_remaining = 4
+INITIAL_GUESSES = 6
+num_of_chances_remaining = INITIAL_GUESSES
+score = 0
 # joke = 'What does an angry pepper do?'
 # punchline = 'It gets jalopeno face.'
+
+# in-game messages
+thanks = "Thanks for playing!"
+start = "Start a new game(y or n)?: "
+keep_going = "Do you want to continue (y or n)?: "
+winner = '\nNice Job! Laugh Away!!\n'
+new_score = '\nYour currant score: '
+chances = '\nNumber of guesses remaining...'
+wrong = "\nIncorrect guesses: "
+lose = '\nClose but no cigar\n'
 
 
 def get_joke():
@@ -33,12 +46,12 @@ def get_joke():
             punchline = joke_match.group(2).strip()
             return joke, punchline
     # discard joke if there is no distinct punchline
-        print(
-            f'Did not get a good joke. {joke_requests_remaining} tries left.')
+        print('Did not get a good joke from API.')
     sys.exit(1)
 
 
 def update_blanks(punchline):
+    """display the gameboard"""
     display = []
     for char in punchline.lower():
         if not char.isalpha() or char in guessed_letters:
@@ -49,23 +62,51 @@ def update_blanks(punchline):
     return ' '.join(display)
 
 
+def continue_game(msg):
+    """continue or reset game"""
+    choice = input(msg)
+    if choice not in ('y', 'n'):
+        continue_game(msg)
+    return True if choice == 'y' else False
+
+
+def init(lost=False):
+    """Initialize all game settings"""
+    global num_of_chances_remaining, score
+    num_of_chances_remaining = INITIAL_GUESSES
+    guessed_letters.clear()
+    wrong_letters.clear()
+    if lost:
+        score = 0
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main():
-    num_of_chances_remaining = 6
-    joke = get_joke()
-    text, punchline = joke
+    global num_of_chances_remaining, score
+    init()
+    joke, punchline = get_joke()
 
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'\n{text}\n')
+        clear_screen()
+        print(f'{new_score}{score}')
+        print(f'\n{joke}\n')
         gameboard = update_blanks(punchline)
         if '_' not in gameboard:
-            print(punchline)
-            print(f'\nNice Job! Laugh Away!!\n')
-            break
-        print(gameboard)
-        print(f'\nNumber of guesses remaining...{num_of_chances_remaining}\n')
-        print(body_dict[num_of_chances_remaining])
-        print(f"\nIncorrect guesses: {' '.join(wrong_letters)}")
+            score += (100 // INITIAL_GUESSES) * num_of_chances_remaining
+            print(f'{punchline}\n{winner}\n{new_score}{score}\n')
+            if continue_game(keep_going):
+                init()
+                joke, punchline = get_joke()
+                continue
+            else:
+                print(thanks)
+                break
+        print(f'{gameboard}\n')
+        print(f"{chances}{num_of_chances_remaining}\n{wrong}{' '.join(wrong_letters)}\n")
+        print(f'{body_dict[num_of_chances_remaining]}\n')
         guess = input("Guess a letter: ").lower()
         if guess not in punchline.lower():
             wrong_letters.append(guess)
@@ -74,10 +115,14 @@ def main():
         if guess not in guessed_letters:
             guessed_letters.append(guess)
         if not num_of_chances_remaining:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(body_dict[num_of_chances_remaining])
-            print(f'\nClose but no cigar\n')
-            break
+            clear_screen()
+            print(f'{body_dict[num_of_chances_remaining]}\n{lose}')
+            if continue_game(start):
+                init(lost=True)
+                joke, punchline = get_joke()
+            else:
+                print(thanks)
+                break
 
 
 if __name__ == '__main__':
